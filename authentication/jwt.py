e3ad8bb9aa3a009a494f5c10efc392cf07073c1a
ragnar <zackmwangi998@gmail.com>
2023-04-30 13:58:14 +0300

    fix authentication bug

--- authentication/jwt.py
+++ authentication/jwt.py
@@ -1,8 +1,11 @@
+import jwt
 from rest_framework.authentication import get_authorization_header, BaseAuthentication
+from authentication.models import User
+
 from rest_framework import exceptions
 import jwt
+
 from django.conf import settings
-from .models import User
 
 
 class JWTAuthentication(BaseAuthentication):
@@ -10,29 +13,36 @@ class JWTAuthentication(BaseAuthentication):
     def authenticate(self, request):
 
         auth_header = get_authorization_header(request)
+
         auth_data = auth_header.decode('utf-8')
+
         auth_token = auth_data.split(" ")
 
-        if len(auth_token)!=2:
-            raise exceptions.AuthenticationFailed('Token not valid!!')
-        
+        if len(auth_token) != 2:
+            raise exceptions.AuthenticationFailed('Token not valid')
+
         token = auth_token[1]
 
         try:
-            payload = jwt.decode(token,settings.SECRET_KEY,algorithm='HS256')
+            payload = jwt.decode(
+                token, settings.SECRET_KEY, algorithms="HS256")
 
-            user = User.objects.get(username=payload['username'])
+            username = payload['username']
 
+            user = User.objects.get(username=username
+                                    )
             return (user, token)
 
-        except jwt.ExpiredSignatureError as er:
-            raise exceptions.AuthenticationFailed('Token Expired!!')
-        except jwt.DecodeError as er:
-            raise exceptions.AuthenticationFailed('Token is Invalid!!')
-        except User.DoesNotExist as no_user:
-            raise exceptions.AuthenticationFailed('No such user!!')
+        except jwt.ExpiredSignatureError as ex:
+            raise exceptions.AuthenticationFailed(
+                'Token is expired, login again')
 
+        except jwt.DecodeError as ex:
+            raise exceptions.AuthenticationFailed(
+                'Token is invalid,')
+
+        except User.DoesNotExist as no_user:
+            raise exceptions.AuthenticationFailed(
+                'No such user')
 
         return super().authenticate(request)
-
-
